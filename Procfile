ff1267e00867047e7491b111f20345ab839e89f0
Rui-1Lovers <zhangcungi@gmail.com>
2025-06-29 19:39:24 +0700

    add Procfile and update requirements.txt

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn rental.wsgi:application --bind 0.0.0.0:$PORT
--- rental/settings.py
+++ rental/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
 ]
 
 AUTH_USER_MODEL = 'rental_app.User'
+ALLOWED_HOSTS = ['*']
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
